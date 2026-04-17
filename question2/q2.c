6b8d079088fd21445626b1bd1063008a6d93e062
InfinitesimalSweven <natgarg132@gmail.com>
2026-04-16 20:57:34 -0400

    Final

--- question2/q2.c
+++ question2/q2.c
@@ -8,7 +8,7 @@ typedef struct Node {
     int count;
     struct Node *left;
     struct Node *right;
-} Node;
+} Node; //creates the Node struct
 
 Node *insert(Node *root, char *word, int count) {
     if (root == NULL) {
@@ -24,15 +24,15 @@ Node *insert(Node *root, char *word, int count) {
         root->right = insert(root->right, word, count);
 
     return root;
-}
+} //function to insert a node
 
-// inorder traversal prints least to greatest
 void inorder(Node *root) {
     if (root == NULL) return;
     inorder(root->left);
     printf("%s: %d\n", root->word, root->count);
     inorder(root->right);
-}
+}// inorder traversal prints least to greatest
+
 
 int finder(char **words, int *counts, char *word, int *unique) {
     for (int i = 0; i < *unique; i++) {
@@ -45,27 +45,26 @@ int finder(char **words, int *counts, char *word, int *unique) {
     words[*unique] = strdup(word);
     counts[(*unique)++] = 1;
     return 1;
-}
+} //This function was intialloy intended to just find the number of unique words but now it also finds how many of the word is in the function
 
 int main() {
     FILE *fptr = stdin;
     char line[256];
     int wordCount = 0;
 
-    // count words
     while (fgets(line, sizeof(line), fptr) != NULL) {
         char *word = strtok(line, " \t\n");
         while (word != NULL) {
             wordCount++;
             word = strtok(NULL, " \t\n");
         }
-    }
+    } //Finds the number of words
 
     rewind(fptr);  // go back to start
 
     char* uniWords[wordCount];
     int uniCount[wordCount];
-    memset(uniCount, 0, wordCount);
+    memset(uniCount, 0, wordCount); //set uniCount all to zero 
 
     int unique = 0;
 
@@ -75,13 +74,14 @@ int main() {
             finder(uniWords, uniCount, word, &unique);
             word = strtok(NULL, " \t\n");
         }
-    }
+    } //Checks every word and finds the total counts
 
     Node *root = NULL;
-    for (int i = 0; i < unique; i++)
-        root = insert(root, uniWords[i], uniCount[i]);
 
-    inorder(root);
+    for (int i = 0; i < unique; i++)
+        root = insert(root, uniWords[i], uniCount[i]); //insert every element into the tree
+
+    inorder(root); //print the tree in order
 
 
 }
